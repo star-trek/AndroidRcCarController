84020fedeb417d8b6db48284ab56b9a4265519e7
NYC <youngchaena@naver.com>
2012-11-16 22:06:10 +0900

    GPS. Still working. need server push

--- src/gdg/androidrccarcontroller/ControllerActivity.java
+++ src/gdg/androidrccarcontroller/ControllerActivity.java
@@ -1,6 +1,7 @@
 package gdg.androidrccarcontroller;
 
 import java.io.ByteArrayOutputStream;
+import java.util.Locale;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -9,8 +10,13 @@ import gdg.androidrccarcontroller.tool.BluetoothConnector;
 import gdg.androidrccarcontroller.tool.ServerCallback;
 import gdg.androidrccarcontroller.tool.ServerConnector;
 import gdg.androidrccarcontroller.view.CameraSurfaceView;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Context;
 import android.content.pm.ActivityInfo;
 import android.graphics.Rect;
 import android.graphics.YuvImage;
@@ -20,7 +26,7 @@ import android.util.Log;
 import android.view.Menu;
 import android.widget.LinearLayout;
 
-public class ControllerActivity extends Activity implements PreviewCallback, ServerCallback{
+public class ControllerActivity extends Activity implements PreviewCallback, ServerCallback, LocationListener{
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -34,7 +40,12 @@ public class ControllerActivity extends Activity implements PreviewCallback, Ser
         linearLayout.addView(cameraSurfaceView);
         BluetoothConnector.getInstance().open(this);
         ServerConnector.getInstance().setServerCallback(this);
+        //TODO Get GPS
+        getGPS();
+        //gdg.androidrccarcontroller.tool.gps.GetLocations();
     }
+    
+    
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -81,6 +92,56 @@ public class ControllerActivity extends Activity implements PreviewCallback, Ser
 			e.printStackTrace();
 		}
 	}
+
+	//code about gps
+	public void getGPS(){
+    	LocationManager location = null;
+    	Location mlocation = null;
+    	double lat = 0;
+    	double lng = 0;
+    	Geocoder geoCorder;
+    	
+    	location = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+        //GPS위치정보 업데이트
+        location.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
+        //기지국으로 부터 업데이트
+        geoCorder = new Geocoder(this, Locale.KOREA);
+        
+        if(mlocation != null){
+    		lat = mlocation.getLatitude();
+    		lng = mlocation.getLongitude();
+    		Log.d("gps", "success");
+    		Log.d("gps", " "+lat);
+    		Log.d("gps", ""+lng);
+    		
+    	}
+    }
+	
+	@Override
+	public void onLocationChanged(Location location) {
+		// TODO Auto-generated method stub
+		Log.d("location", "location changed");
+		this.getGPS().mlocation = location;
+	}
+
+	@Override
+	public void onProviderDisabled(String provider) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onProviderEnabled(String provider) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onStatusChanged(String provider, int status, Bundle extras) {
+		// TODO Auto-generated method stub
+		
+	}
+	//related with gps.
 
     
 }
